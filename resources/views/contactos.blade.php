777e46038a37624af88d9eeb53a28f32ab762ecd
AgostinhoMartins <AgostinhoMartins@bitbucket.org>
2019-05-12 14:45:13 +0100

    Página contactos e sobre nós

---
+++ resources/views/contactos.blade.php
@@ -0,0 +1,2 @@
+<h1>Pagina de contactos (Contactos) do website da empresa AGO</h1>
+<h3>Em construção - Brevemente estaremos online</h3>
--- resources/views/sobre-nos.blade.php
+++ resources/views/sobre-nos.blade.php
@@ -1,2 +1,2 @@
-<h1>Pagina de informação "SOBRE NÓS" do website da empresa AGO</h1>
+<h1>Pagina de informação (SOBRE NÓS) do website da empresa AGO</h1>
 <h3>Em construção - Brevemente estaremos online</h3>
